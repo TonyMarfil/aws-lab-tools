14538f9fb70a76206ce3d0bd627d5c55e6753686
Tony Marfil <tonymarfil@gmail.com>
2017-08-08 22:07:51 -0400

    added autopilot.sh timestamps

--- autopilot.sh
+++ autopilot.sh
@@ -4,6 +4,7 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 SECONDS=0
+duration=$SECONDS
 
 # install figlet
 apk add figlet
@@ -16,15 +17,17 @@ cd ~/marfil-f5-terraform/
 source ./start
 terraform plan
 terraform apply
-duration=$SECONDS
+
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 sleep 15m
+
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
 $(lab-info 2>/dev/null | grep ssh -m2 | tail -n1 | cut -c12-) < /bigiptest.sh
 $(lab-info 2>/dev/null | grep ssh -m3 | tail -n1 | cut -c12-) < /bigiptest.sh
 sleep 15m
+
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
@@ -32,11 +35,13 @@ $(lab-info 2>/dev/null | grep ssh -m2 | tail -n1 | cut -c12-) < /bigiptest.sh
 $(lab-info 2>/dev/null | grep ssh -m3 | tail -n1 | cut -c12-) < /bigiptest.sh
 
 figlet Triggering autoscale WAF...
+
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 wafUrl=$(lab-info | grep "WAF ELB" -A 2 | tail -n2 | cut -c8-)
 base64 /dev/urandom | head -c 3000 > payload
 ab -t 180 -c 200 -c 5 -T 'multipart/form-data; boundary=1234567890' -p payload $wafUrl/
 sleep 15m
+
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
