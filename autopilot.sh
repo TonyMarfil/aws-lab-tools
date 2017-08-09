5a9f3f9a1ff3a50fded4337f2ea93b73462ba637
Tony Marfil <tonymarfil@gmail.com>
2017-08-09 13:45:21 -0400

    fixed duration=

--- autopilot.sh
+++ autopilot.sh
@@ -4,7 +4,6 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 SECONDS=0
-duration=$SECONDS
 
 # install figlet
 apk add figlet
@@ -18,9 +17,11 @@ source ./start
 terraform plan
 terraform apply
 
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 sleep 15m
 
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
@@ -28,6 +29,7 @@ $(lab-info 2>/dev/null | grep ssh -m2 | tail -n1 | cut -c12-) < /bigiptest.sh
 $(lab-info 2>/dev/null | grep ssh -m3 | tail -n1 | cut -c12-) < /bigiptest.sh
 sleep 15m
 
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
@@ -36,12 +38,14 @@ $(lab-info 2>/dev/null | grep ssh -m3 | tail -n1 | cut -c12-) < /bigiptest.sh
 
 figlet Triggering autoscale WAF...
 
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 wafUrl=$(lab-info | grep "WAF ELB" -A 2 | tail -n2 | cut -c8-)
 base64 /dev/urandom | head -c 3000 > payload
 ab -t 180 -c 200 -c 5 -T 'multipart/form-data; boundary=1234567890' -p payload $wafUrl/
 sleep 15m
 
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 lab-info
 $(lab-info 2>/dev/null | grep ssh -m1 | tail -n1 | cut -c12-) < /bigiptest.sh
